cf34dbf3264834e7afcb184cf4138c0906f9f9d8
Le Thanh Trung <trung.le@navigosgroup.com>
2022-11-06 10:56:13 +0700

    [Update]

---
+++ README.md
@@ -0,0 +1,23 @@
+# Dataset
+
+- http://205.174.165.80/CICDataset/CIC-IDS-2017/Dataset/
+
+# Refer
+
+- https://www.kaggle.com/code/azazurrehmanbutt/cicids-ids-2018-using-cnn
+
+
+
+# Prepare
+
+- The `python 3.10` is used for this lab
+- Install pipenv
+  - Macos: `brew install pipenv`
+  - Debian: `apt install pipenv`
+- Install dependencies: `pipenv install`
+- Download dataset: 
+```
+wget http://205.174.165.80/CICDataset/CIC-IDS-2017/Dataset/MachineLearningCSV.zip   
+wget http://205.174.165.80/CICDataset/CIC-IDS-2017/Dataset/GeneratedLabelledFlows.zip
+```
+# Development
--- src/main.py
+++ src/main.py
@@ -105,6 +105,93 @@ def circle(network_data):
     p = plt.gcf()
     p.gca().add_artist(circle)
 
+
+    test_dataset = train_dataset.sample(frac=0.1)
+    target_train = train_dataset['Label']
+    target_test = test_dataset['Label']
+    target_train.unique(), target_test.unique()
+
+    y_train = to_categorical(target_train, num_classes=3)
+    y_test = to_categorical(target_test, num_classes=3)
+
+    train_dataset = train_dataset.drop(columns = ["Timestamp", "Protocol","PSH Flag Cnt","Init Fwd Win Byts","Flow Byts/s","Flow Pkts/s", "Label"], axis=1)
+    test_dataset = test_dataset.drop(columns = ["Timestamp", "Protocol","PSH Flag Cnt","Init Fwd Win Byts","Flow Byts/s","Flow Pkts/s", "Label"], axis=1)
+
+    X_train = train_dataset.iloc[:, :-1].values
+    X_test = test_dataset.iloc[:, :-1].values
+    X_test.show()
+
+    print(X_train.shape)
+    print(X_test.shape)
+    print(y_train.shape)
+    print(y_test.shape)
+
+    # reshape the data for CNN
+    X_train = X_train.reshape(len(X_train), X_train.shape[1], 1)
+    X_test = X_test.reshape(len(X_test), X_test.shape[1], 1)
+    X_train.shape, X_test.shape
+    model = Model()
+    model.summary()
+    logger = CSVLogger('logs.csv', append=True)
+    his = model.fit(X_train, y_train, epochs=30, batch_size=32, 
+          validation_data=(X_test, y_test), callbacks=[logger])
+    scores = model.evaluate(X_test, y_test)
+    print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
+    history = his.history
+    history.keys()
+    epochs = range(1, len(history['loss']) + 1)
+    acc = history['accuracy']
+    loss = history['loss']
+    val_acc = history['val_accuracy']
+    val_loss = history['val_loss']
+
+    # visualize training and val accuracy
+    plt.figure(figsize=(10, 5))
+    plt.title('Training and Validation Accuracy (CNN)')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.plot(epochs, acc, label='accuracy')
+    plt.plot(epochs, val_acc, label='val_acc')
+    plt.legend()
+
+    # visualize train and val loss
+    plt.figure(figsize=(10, 5))
+    plt.title('Training and Validation Loss(CNN)')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.plot(epochs, loss, label='loss', color='g')
+    plt.plot(epochs, val_loss, label='val_loss', color='r')
+    plt.legend()
+    
+
+def Model():
+    model = Sequential()
+    model.add(Conv1D(filters=64, kernel_size=6, activation='relu', 
+                    padding='same', input_shape=(72, 1)))
+    model.add(BatchNormalization())
+    
+    # adding a pooling layer
+    model.add(MaxPooling1D(pool_size=(3), strides=2, padding='same'))
+    
+    model.add(Conv1D(filters=64, kernel_size=6, activation='relu', 
+                    padding='same', input_shape=(72, 1)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size=(3), strides=2, padding='same'))
+    
+    model.add(Conv1D(filters=64, kernel_size=6, activation='relu', 
+                    padding='same', input_shape=(72, 1)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size=(3), strides=2, padding='same'))
+    
+    model.add(Flatten())
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(3, activation='softmax'))
+    
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
 def main(path):
     network_data = loading_data(path)
     validate(network_data)
@@ -113,13 +200,13 @@ def main(path):
     print(network_data.info())
     print(network_data['Label'].value_counts())
 
-    plot_number(network_data)
-    scatterplot(network_data)
+    # plot_number(network_data)
+    # scatterplot(network_data)
     circle(network_data)
-
+    
 
 if __name__ == '__main__':
-    path = "/Users/TrungLT/CNN/TrafficLabelling/"
+    path = "/Users/TrungLT/CNN/Input/"
 
     if os.path.isfile(path):
         main(path)
