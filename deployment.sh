d0bdbd8dbdeb12b1f1799c9c588f51d75c715894
Le Thanh Trung <trung.le@navigosgroup.com>
2022-11-08 07:30:47 +0700

    update

--- deployment.sh
+++ deployment.sh
@@ -1,3 +1,3 @@
 docker rm -f lab || echo "Not found"
 docker build . -t lab
-docker run --rm -d -p 8888:8888 --name lab -v /opt/IDS-CNN/lab:/home/jovyan/work:rw -v /data/input:/input:rw lab
+docker run --rm -d -p 80:8888 --name lab -v /opt/IDS-CNN/lab:/home/jovyan/work:rw -v /data/input:/input:rw lab
