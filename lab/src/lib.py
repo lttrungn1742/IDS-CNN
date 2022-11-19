46d31ebf77928c5f5e954169d358c9f0027af496
Le Thanh Trung <trung.le@navigosgroup.com>
2022-11-19 13:52:41 +0700

    update

--- lab/src/lib.py
+++ lab/src/lib.py
@@ -1,6 +1,7 @@
-import numpy as np
-import pandas as pd 
-import os, re, time, math, tqdm, itertools, sys
+# import libraries
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import os, re, time, math, tqdm, itertools
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
@@ -10,222 +11,13 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.neural_network import MLPClassifier
-import keras
 from keras.layers import Conv2D, Conv1D, MaxPooling2D, MaxPooling1D, Flatten, BatchNormalization, Dense
 from keras.utils.np_utils import to_categorical
 from keras.models import Sequential
 from keras.callbacks import CSVLogger, ModelCheckpoint
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 from sklearn.utils import resample
 
 default_path = os.getenv('CNN_LAB_PATH',"/Users/TrungLT/personal/CNN/Input")
-
-def _scatter_plot(network_data, vertical_axis_label, horizontal_axis_label):
-    pyo.init_notebook_mode(connected=True)
-    fig = px.scatter(x=network_data[vertical_axis_label][:100000],
-                     y=network_data[horizontal_axis_label][:100000])
-    fig.show()
-
-    sns.set(rc={'figure.figsize':(12, 6)})
-    plt.xlabel(horizontal_axis_label)
-    sns.set_theme()
-    ax = sns.scatterplot(x=network_data[vertical_axis_label][:50000], y=network_data[horizontal_axis_label][:50000], 
-                hue='Label', data=network_data)
-    ax.set(xlabel=vertical_axis_label, ylabel=horizontal_axis_label)   
-    plt.show()
-
-
-
-def scatter_plot(path=default_path, isShowInfomation=False, vertical_axis_label="Bwd Packets/s", horizontal_axis_label="Fwd Packet Length Min"):
-    if os.path.isfile(path):
-        network_data = get_network_data(path, isShowInfomation=isShowInfomation)
-        _scatter_plot(network_data, vertical_axis_label, horizontal_axis_label)
-
-    elif os.path.isdir(path):
-        for dirname, _, filenames in os.walk(path):
-            for filename in filenames:
-                file = os.path.join(dirname, filename)
-                print(f"-  {file}")
-                try:
-                    network_data = get_network_data(file, isShowInfomation=isShowInfomation)
-                    _scatter_plot(network_data, vertical_axis_label, horizontal_axis_label)
-                except Exception as err:
-                    print(err)
-    else:
-        print("The path is not file or directory")
-
-
-
-def sanitize(column):
-    while ' ' == column[0]:
-        column = column[1:]
-    return column
-
-def _plot_number(network_data):
-    sns.set(rc={'figure.figsize': (12, 6)})
-    plt.xlabel('Attack Type')
-    sns.set_theme()
-    ax = sns.countplot(x='Label', data=network_data)
-    ax.set(xlabel='Attack Type', ylabel='Number of Attacks')
-    plt.show()
-
-def plot_number(path = default_path, isShowInfomation = False):
-    if os.path.isfile(path):
-        network_data = get_network_data(path, isShowInfomation = isShowInfomation)
-        _plot_number(network_data)
-
-    elif os.path.isdir(path):
-        for dirname, _, filenames in os.walk(path):
-            for filename in filenames:
-                file = os.path.join(dirname, filename)
-                print(f"-  {file}")
-                try:
-                    network_data = get_network_data(file, isShowInfomation = isShowInfomation)
-                    _plot_number(network_data)
-                except Exception as err:
-                    print(err)
-    else:
-        print("The path is not file or directory")
-
-def _circle(network_data):
-    cleaned_data = network_data.dropna()
-    cleaned_data.isna().sum().to_numpy()
-    label_encoder = LabelEncoder()
-    cleaned_data['Label']= label_encoder.fit_transform(cleaned_data['Label'])
-    cleaned_data['Label'].unique()
-    cleaned_data['Label'].value_counts()
-    data_1 = cleaned_data[cleaned_data['Label'] == 0]
-    data_2 = cleaned_data[cleaned_data['Label'] == 1]
-    data_3 = cleaned_data[cleaned_data['Label'] == 2]
-
-    # make benign feature
-    y_1 = np.zeros(data_1.shape[0])
-    y_benign = pd.DataFrame(y_1)
-
-    # make bruteforce feature
-    y_2 = np.ones(data_2.shape[0])
-    y_bf = pd.DataFrame(y_2)
-
-    # make bruteforceSSH feature
-    y_3 = np.full(data_3.shape[0], 2)
-    y_ssh = pd.DataFrame(y_3)
-
-    # merging the original dataframe
-    X = pd.concat([data_1, data_2, data_3], sort=True)
-    y = pd.concat([y_benign, y_bf, y_ssh], sort=True)
-    data_1_resample = resample(data_1, n_samples=20000, random_state=123, replace=True)
-    data_2_resample = resample(data_2, n_samples=20000, random_state=123, replace=True)
-    data_3_resample = resample(data_3, n_samples=20000, random_state=123, replace=True)
-    train_dataset = pd.concat([data_1_resample, data_2_resample, data_3_resample])
-    train_dataset.head(2)
-    plt.figure(figsize=(10, 8))
-    circle = plt.Circle((0, 0), 0.7, color='white')
-    plt.title('Intrusion Attack Type Distribution')
-    plt.pie(train_dataset['Label'].value_counts(), labels=['Benign', 'BF', 'BF-SSH'], colors=['blue', 'magenta', 'cyan'])
-    p = plt.gcf()
-    p.gca().add_artist(circle)
-
-    test_dataset = train_dataset.sample(frac=0.1)
-    target_train = train_dataset['Label']
-    target_test = test_dataset['Label']
-    target_train.unique(), target_test.unique()
-
-    y_train = to_categorical(target_train, num_classes=3)
-    y_test = to_categorical(target_test, num_classes=3)
-
-    train_dataset = train_dataset.drop(columns = ["Timestamp", "Protocol","PSH Flag Cnt","Init Fwd Win Byts","Flow Byts/s","Flow Pkts/s", "Label"], axis=1)
-    test_dataset = test_dataset.drop(columns = ["Timestamp", "Protocol","PSH Flag Cnt","Init Fwd Win Byts","Flow Byts/s","Flow Pkts/s", "Label"], axis=1)
-
-    X_train = train_dataset.iloc[:, :-1].values
-    X_test = test_dataset.iloc[:, :-1].values
-    X_test.show()
-
-    print(X_train.shape)
-    print(X_test.shape)
-    print(y_train.shape)
-    print(y_test.shape)
-
-    # reshape the data for CNN
-    X_train = X_train.reshape(len(X_train), X_train.shape[1], 1)
-    X_test = X_test.reshape(len(X_test), X_test.shape[1], 1)
-    X_train.shape, X_test.shape
-    model = Model()
-    model.summary()
-    logger = CSVLogger('logs.csv', append=True)
-    his = model.fit(X_train, y_train, epochs=30, batch_size=32, 
-          validation_data=(X_test, y_test), callbacks=[logger])
-    scores = model.evaluate(X_test, y_test)
-    print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
-    history = his.history
-    history.keys()
-    epochs = range(1, len(history['loss']) + 1)
-    acc = history['accuracy']
-    loss = history['loss']
-    val_acc = history['val_accuracy']
-    val_loss = history['val_loss']
-
-    # visualize training and val accuracy
-    plt.figure(figsize=(10, 5))
-    plt.title('Training and Validation Accuracy (CNN)')
-    plt.xlabel('Epochs')
-    plt.ylabel('Accuracy')
-    plt.plot(epochs, acc, label='accuracy')
-    plt.plot(epochs, val_acc, label='val_acc')
-    plt.legend()
-
-    # visualize train and val loss
-    plt.figure(figsize=(10, 5))
-    plt.title('Training and Validation Loss(CNN)')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.plot(epochs, loss, label='loss', color='g')
-    plt.plot(epochs, val_loss, label='val_loss', color='r')
-    plt.legend()
-
-def circle(path = default_path, isShowInfomation = False):
-    if os.path.isfile(path):
-        network_data = get_network_data(path, isShowInfomation = isShowInfomation)
-        _circle(network_data)
-
-    elif os.path.isdir(path):
-        for dirname, _, filenames in os.walk(path):
-            for filename in filenames:
-                file = os.path.join(dirname, filename)
-                print(f"-  {file}")
-                try:
-                    network_data = get_network_data(file, isShowInfomation = isShowInfomation)
-                    _circle(network_data)
-                except Exception as err:
-                    print(err)
-    else:
-        print("The path is not file or directory")    
-
-def Model():
-    model = Sequential()
-    model.add(Conv1D(filters=64, kernel_size=6, activation='relu', 
-                    padding='same', input_shape=(72, 1)))
-    model.add(BatchNormalization())
-    
-    # adding a pooling layer
-    model.add(MaxPooling1D(pool_size=(3), strides=2, padding='same'))
-    
-    model.add(Conv1D(filters=64, kernel_size=6, activation='relu', 
-                    padding='same', input_shape=(72, 1)))
-    model.add(BatchNormalization())
-    model.add(MaxPooling1D(pool_size=(3), strides=2, padding='same'))
-    
-    model.add(Conv1D(filters=64, kernel_size=6, activation='relu', 
-                    padding='same', input_shape=(72, 1)))
-    model.add(BatchNormalization())
-    model.add(MaxPooling1D(pool_size=(3), strides=2, padding='same'))
-    
-    model.add(Flatten())
-    model.add(Dense(64, activation='relu'))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dense(3, activation='softmax'))
-    
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    return model
 
 def sanitize(column):
     while ' ' == column[0]:
@@ -245,15 +37,58 @@ def validate(network_data):
         columns = [sanitize(i) for i in network_data.columns]
         network_data.columns = columns
 
-def get_network_data(path, isShowInfomation=True):
+def get_network_data(path):
     network_data = loading_data(path)
     validate(network_data)
-    if isShowInfomation:
-        print(network_data.shape)
-        print(network_data.info())
-        print(network_data['Label'].value_counts())
     return network_data
     
+def main(path, x_header_networkdata="Bwd Packets/s", y_header_networkdata="min_seg_size_forward"):
+    # Load dataset
+    network_data = get_network_data(path)
+    
+    # Show information of network data
+    print(network_data.shape)
+    print('Number of Rows (Samples): %s' % str((network_data.shape[0])))
+    print('Number of Columns (Features): %s' % str((network_data.shape[1])))
+    network_data.head(4)
+    print(network_data.columns)
+    print(network_data.info())
+    print(network_data['Label'].value_counts())
+
+    sns.set(rc={'figure.figsize':(12, 6)})
+    plt.xlabel('Attack Type')
+    sns.set_theme()
+    ax = sns.countplot(x='Label', data=network_data)
+    ax.set(xlabel='Attack Type', ylabel='Number of Attacks')
+    plt.show()
+
+    pyo.init_notebook_mode()
+    fig = px.scatter(x = network_data[x_header_networkdata][:100000], 
+                    y=network_data[y_header_networkdata][:100000])
+    fig.show()
+
+    sns.set(rc={'figure.figsize':(12, 6)})
+    sns.scatterplot(x=network_data[x_header_networkdata][:50000],
+                     y=network_data[y_header_networkdata][:50000], 
+                hue='Label', data=network_data)
+
+    # check the dtype of timestamp column
+    (network_data['Timestamp'].dtype)
+
+    # check for some null or missing values in our dataset
+    network_data.isna().sum().to_numpy()
+
+    # drop null or missing columns
+    cleaned_data = network_data.dropna()
+    cleaned_data.isna().sum().to_numpy()
+
+    # encode the column labels
+    label_encoder = LabelEncoder()
+    cleaned_data['Label']= label_encoder.fit_transform(cleaned_data['Label'])
+    cleaned_data['Label'].unique()
+
+        # check for encoded labels
+    cleaned_data['Label'].value_counts()
 
 def handler():
     path = default_path
